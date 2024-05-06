67bb498b51b51a75eb5bb78dc52db74286f1699b
ghadasherif <135965326+ghadasherif@users.noreply.github.com>
2024-05-06 21:12:11 +0300

    ghada's final commit

--- dairy.h
+++ dairy.h
@@ -22,6 +22,24 @@ private slots:
 
     void on_pushButton_Searchdairy_clicked();
 
+    void on_pushButton_adddairyproduct_2_clicked();
+
+    void on_pushButton_adddairyproduct_3_clicked();
+
+    void on_pushButton_adddairyproduct_4_clicked();
+
+    void on_pushButton_adddairyproduct_5_clicked();
+
+    void on_pushButton_adddairyproduct_6_clicked();
+
+    void on_pushButton_adddairyproduct_7_clicked();
+
+    void on_pushButton_adddairyproduct_8_clicked();
+
+    void on_pushButton_adddairyproduct_9_clicked();
+
+    void on_pushButton_adddairyproduct_10_clicked();
+
 private:
     Ui::Dairy *ui;
 };
